c5f8febe7a7534f8a8c879e2dac77d67aa52739c
twolight <twolight@163.com>
2015-10-25 22:48:25 +0800

    Add CountDownLatch

---
+++ CountDownLatch.cpp
@@ -0,0 +1,34 @@
+//
+//  CountDownLatch.cpp
+//  testproject
+//
+//  Created by twolight on 15-10-25.
+//
+//
+
+#include "CountDownLatch.h"
+
+CountDownLatch::CountDownLatch(int count):
+_count(count),
+_mutex(),
+_condition(){
+    
+}
+void CountDownLatch::countDown(){
+    std::unique_lock<std::mutex> lock(_mutex);
+    _count--;
+    if(_count == 0){
+        _condition.notify_all();
+    }
+    lock.unlock();
+}
+int CountDownLatch::getCount(){
+    std::unique_lock<std::mutex> lock(_mutex);
+    return _count;
+}
+void CountDownLatch::wait(){
+    std::unique_lock<std::mutex> lock(_mutex);
+    while(_count > 0){
+        _condition.wait(lock);
+    }
+}
