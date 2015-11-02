0d59174fed0b021375864e0a4ef67ffc0f9f6bf4
twolight <twolight@163.com>
2015-11-02 17:39:24 +0800

    Fixs crash in cocos2d-x loading resource

--- Task.cpp
+++ Task.cpp
@@ -8,6 +8,10 @@
 
 #include "Task.h"
 #include "LogerProxy.h"
+#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+#include "platform/android/jni/JniHelper.h"
+#endif
+
 using namespace std;
 Task::Task(const std::string& name,TaskRunnable& runnable):
 _statu(Statu::PENDING),
@@ -26,6 +30,17 @@ Task::~Task(){
 void Task::start(){
     _statu = Statu::RUNNING;
     _thread = new std::thread([this](){
+        
+        
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+        JavaVM *vm;
+        JNIEnv *env;
+        vm = JniHelper::getJavaVM();
+        
+        vm->AttachCurrentThread(&env, NULL);
+#elif CC_TARGET_PLATFORM == CC_PLATFORM_IOS
+        auto pool = cocos2d::ThreadHelper::createAutoreleasePool();
+#endif
         struct timeval start, end;
         gettimeofday(&start, nullptr);
         _runnable();
@@ -33,6 +48,11 @@ void Task::start(){
         LogerProxy::printf("%s runs: %d millis",getName().c_str(),
                            (long long)(end.tv_sec-start.tv_sec)*1000+(end.tv_usec-start.tv_usec)/1000);
         _doneSignal->countDown();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+        vm->DetachCurrentThread();
+#elif CC_TARGET_PLATFORM == CC_PLATFORM_IOS
+        ThreadHelper::releaseAutoreleasePool(pool);
+#endif
     });
     _thread->detach();
 }
